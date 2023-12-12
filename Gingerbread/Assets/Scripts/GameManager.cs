23082197f147d5729dfc9e06f9e07f941ccd8eb6
Delilah <delstuff@gmail.com>
2023-12-12 23:06:11 +0100

    AssetBundle POC for the world + WIP world prefab

--- Gingerbread/Assets/Scripts/GameManager.cs
+++ Gingerbread/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using System.IO;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,12 +12,19 @@ public class GameManager : MonoBehaviour
     public GameState gameState;
 
     [SerializeField] private GameObject _mainMenuPanel;
+    [SerializeField] private GameObject _pedestalCharacter;
     [SerializeField] private GameObject _backToMenuButton;
 
     // List my input actions
     private PlayerInput _playerInput;
     private InputAction _backToMenuAction;
     private InputAction _exitGameAction;
+
+    private GameObject _worldPrefab;
+    private GameObject _worldInstance;
+    private AssetBundle myLoadedAssetBundle;
+    private Coroutine _loadAssetBundleCoroutine;
+
 
 
     private void Awake()
@@ -38,7 +46,9 @@ public class GameManager : MonoBehaviour
         _backToMenuAction = _playerInput.actions["BackToMenu"];
         _exitGameAction = _playerInput.actions["ExitGame"];
 
-        ChangeState(gameState);
+        //ChangeState(gameState);
+       // ChangeState(GameState.MainMenu);
+        StartGame();
     }
 
     public void Update()
@@ -93,16 +103,76 @@ public class GameManager : MonoBehaviour
         if (state == GameState.MainMenu)
         {
             _mainMenuPanel.SetActive(true);
+            _pedestalCharacter.SetActive(true);
             _backToMenuButton.SetActive(false);
+
+            if (_worldInstance != null)
+            {
+                Destroy(_worldInstance);
+                _worldInstance = null;
+            }
+
+            if (myLoadedAssetBundle != null)
+            {
+                myLoadedAssetBundle.Unload(false);
+                myLoadedAssetBundle = null;
+            }
+
+            if (_loadAssetBundleCoroutine != null)
+            {
+                StopCoroutine(_loadAssetBundleCoroutine);
+                _loadAssetBundleCoroutine = null;
+            }
         }
 
         if (state == GameState.Game)
         {
             // Currently not managing Pause/Start New Game, just switching states
+ _loadAssetBundleCoroutine = StartCoroutine(LoadAssetBundleCoroutine());
+
             _mainMenuPanel.SetActive(false);
+            _pedestalCharacter.SetActive(false);
+
             _backToMenuButton.SetActive(true);
+
+           
+
         }
     }
 
-}
 
+    /// <summary>
+    /// This is used to load the assetbundle
+    /// As it's a proof of concept, only the world prefab is being loaded/unloaded
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator LoadAssetBundleCoroutine()
+    {
+        string bundlePath = Path.Combine(Application.streamingAssetsPath, "worldassetbundle");
+        AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(bundlePath);
+
+        yield return request;
+
+        myLoadedAssetBundle = request.assetBundle;
+
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            yield break;
+        }
+
+        AssetBundleRequest prefabRequest = myLoadedAssetBundle.LoadAssetAsync<GameObject>("World");
+
+        yield return prefabRequest;
+
+        _worldPrefab = prefabRequest.asset as GameObject;
+
+        if (_worldPrefab == null)
+        {
+            Debug.Log("Failed to load the World!");
+            yield break;
+        }
+
+        _worldInstance = Instantiate(_worldPrefab);
+    }
+}
