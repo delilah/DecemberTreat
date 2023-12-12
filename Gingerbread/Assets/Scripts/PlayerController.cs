243c3e2769e64fc8a5669ddaa690bed026918424
Delilah <delstuff@gmail.com>
2023-12-12 17:58:05 +0100

    Jump anim added

--- Gingerbread/Assets/Scripts/PlayerController.cs
+++ Gingerbread/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _playerSpeed = 2.0f;
     [SerializeField] private float _jumpHeight = 1.0f;
     [SerializeField] private float _gravityValue = -9.81f;
+    [SerializeField] private float _animationPlayTransitionTime = 0.13f;
+
 
     private CharacterController _controller;
     private PlayerInput _playerInput;
@@ -25,6 +27,7 @@ public class PlayerController : MonoBehaviour
     private Animator _animator;
     private int _movementXAnimParamId;
     private int _movementZAnimParamId;
+    private int _jumpStateId;
 
     private void Start()
     {
@@ -38,6 +41,7 @@ public class PlayerController : MonoBehaviour
         // retrieving the correct IDs
         _movementXAnimParamId = Animator.StringToHash("MovementX");
         _movementZAnimParamId = Animator.StringToHash("MovementZ");
+        _jumpStateId = Animator.StringToHash("Jump");
 
         _movementAction = _playerInput.actions["Movement"];
         _jumpAction = _playerInput.actions["Jump"];
@@ -58,8 +62,13 @@ public class PlayerController : MonoBehaviour
         _animator.SetFloat(_movementXAnimParamId, input.x);
         _animator.SetFloat(_movementZAnimParamId, input.y);
 
-        if (_jumpAction.triggered && _groundedPlayer) _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);
+        if (_jumpAction.triggered && _groundedPlayer)
+        {
+            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);
+            //_animator.SetTrigger(_jumpAnimParamId);
+            _animator.CrossFade(_jumpStateId, _animationPlayTransitionTime);
 
+        }
         _playerVelocity.y += _gravityValue * Time.deltaTime;
         _controller.Move(_playerVelocity * Time.deltaTime);
     }
